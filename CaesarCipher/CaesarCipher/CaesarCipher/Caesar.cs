1a9225150be3fb3780dc5652da4157ff33a39118
SuprajaKatta9 <supraja.katta@klingelnberg.com>
2023-02-22 17:08:29 +0530

    Committing Caesar Cipher program.

---
+++ CaesarCipher/CaesarCipher/CaesarCipher/Caesar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    public class Caesar
+    {
+        public void run()
+        {
+            Console.WriteLine("Enter the string to encrypt");
+      
+
+            string input = Console.ReadLine().Trim();
+            input= input.ToUpper();
+            if(string.IsNullOrEmpty(input))
+            {
+                return;
+            }
+            
+            Console.WriteLine("Enter the encryption value");
+            int i = int.Parse(Console.ReadLine());
+            char []s = input.ToArray();
+         
+           // int shift = 0;
+            for (int j = 0; j < s.Length; j++)
+            {
+                // Console.WriteLine(data1['A']);
+                // int shift = (data1[s[j]] + i) % 26;
+                //  Console.WriteLine(data[shift]);
+                //Console.WriteLine($"{data[shift]}\t");
+                //  
+                var r=(s[j] - 65 + i) % 26;
+                Console.Write(Convert.ToChar(r + 65));
+
+            }
+            
+
+        }
+       
+    }
+}
---
+++ CaesarCipher/CaesarCipher/CaesarCipher/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Caesar c=new Caesar();
+            c.run();
+        }
+
+    }
+}
